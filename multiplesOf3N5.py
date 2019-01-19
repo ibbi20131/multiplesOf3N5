5d86c99991ae8d1dd5a2c805a2439d688d37fc81
ibbi20131 <44727739+ibbi20131@users.noreply.github.com>
2019-01-19 10:51:49 +0400

    Add files via upload

--- multiplesOf3N5.py
+++ multiplesOf3N5.py
@@ -1,6 +1,6 @@
-def multiplesOf3N5():
+def multiplesOf3N5(max):
     count = 0
-    for i in range (1000):
+    for i in range (max):
         if i%3 == 0 or i%5 == 0:
             count = count+i
     return(count)
